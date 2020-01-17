c55983bc79d8363bfb395ce45fe74ee663f96494
WANGWei <wei.wang@etu.unice.fr>
2020-01-17 17:02:35 +0100

    add database

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 const http = require('http').createServer()
+const MongoClient = require('mongodb').MongoClient
+const urlDB = 'mongodb://localhost:27017/tto'
 
 const io = require('socket.io')(http)
+
+MongoClient.connect(urlDB, {useNewUrlParser: true}, function (err, db) {
+  if (err) throw  err
+  console.log('database created')
+  const database = db.db('tto')
+  const quiz = {
+    name: 'quiz 1'
+  }
+  // database.createCollection('quizz', function (err, res) {
+  //   if (err) throw err;
+  //   console.log('collection created')
+  //   db.close();
+  // })
+
+  database.collection('quizz').insertOne(quiz, function (err, res) {
+    if (err) throw err
+    console.log('inserted quiz')
+    db.close()
+  })
+
+})
 
 io.on('connection', (socket) => {
   //Socket is a Link to the Client
@@ -14,6 +37,19 @@ io.on('connection', (socket) => {
     name: 'wangwei',
     machine: 'socketIO'
   }
+
+  MongoClient.connect(urlDB, {useNewUrlParser: true}, function (err, db) {
+    if (err) throw  err
+    const dbo = db.db('tto')
+    dbo.collection('quizz').find({}).toArray(function (err, result) {
+      if (err) throw  err
+      console.log(result)
+      socket.emit('quizz', result)
+      db.close()
+    })
+  })
+
+
   socket.emit('welcome', testJson)
 
   socket.on('message', (data) => {
@@ -24,8 +60,20 @@ io.on('connection', (socket) => {
 
 })
 
-
 //Listen the HTTP Server
 http.listen(port, () => {
   console.log('Server Is Running Port: ' + port)
 })
+
+process.on('SIGINT', () => {
+  console.log('SIGINT: Closing MongoDB connection');
+  MongoClient.connect(urlDB, {useNewUrlParser: true}, function (err, db) {
+    if(err) throw  err;
+    const dbo = db.db('tto');
+    dbo.dropDatabase();
+    dbo.close()
+    console.log('dfsdf')
+    process.exit(0)
+  })
+})
+
