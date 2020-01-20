b43c5ef6b46764f7430cb2d9dd5f2d86fad0bf87
WANGWei <wei.wang@etu.unice.fr>
2020-01-20 13:06:34 +0100

    add server

---
+++ TTO-Server-master/src/index.js
@@ -0,0 +1,27 @@
+/**
+ * @author Christian Brel <ch.brel@gmail.com>
+ */
+
+import SocketIOServer from './SocketIOServer'
+import ClientManager from './ClientManager'
+
+const clientManager = new ClientManager()
+
+const onNewClient = (socket) => {
+  clientManager.addClient(socket)
+}
+
+const onClientDisconnection = (socket) => {
+  clientManager.deleteClient(socket)
+}
+
+const socketIOServer = new SocketIOServer()
+socketIOServer.onNewClient(onNewClient)
+socketIOServer.onClientDisconnection(onClientDisconnection)
+socketIOServer.start()
+
+process.on('SIGINT', () => {
+  console.log('SIGINT: Closing MongoDB connection');
+  socketIOServer.closeDatabase();
+})
+
