58d550539dbea74fde957e4fdaa9891cd02fda38
LiuJiaqi-unice <katherine9677@163.com>
2020-01-23 19:52:40 +0100

    mvp ordi

---
+++ TTO-Client-Ordi/src/socket/client.js
@@ -0,0 +1,22 @@
+import openSocket from 'socket.io-client';
+
+const socket = openSocket('http://192.168.99.1:10000');
+function send(){
+    socket.emit('message','react');
+}
+
+function Client(){
+    socket.on('welcome',(data) =>{
+        console.log(data);
+    })
+    socket.on('message received',(data) => {
+        console.log(data);
+    })
+
+    socket.on('quizz',(data) => {
+        console.log(data);
+    })
+
+}
+
+export default Client();
