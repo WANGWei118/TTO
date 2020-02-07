282c2da1015bddd8b643aec22ade9c7c17c5fcb4
ValentinAh-Kane <valentin.ah-kane@etu.univ-cotedazur.fr>
2020-02-07 11:10:32 +0100

    .

--- TTO-Client-Touchpad/src/component/QuizCard.js
+++ TTO-Client-Touchpad/src/component/QuizCard.js
@@ -29,10 +29,10 @@ const QuizCard = (props) => {
     const defaultImage = () => {
         if (topic === 'animal') {
             return (
-                <img src={"http://192.168.1.11:1000/assets/animals.jpg"} />)
+                <img src={"http://192.168.1.11:10000/assets/animals.jpg"} />)
         }
         else {
-            return (<img src={"http://192.168.1.11:1000/assets/Fruits.jpg"} />)
+            return (<img src={"http://192.168.1.11:10000/assets/Fruits.jpg"} />)
         }
     }
 
