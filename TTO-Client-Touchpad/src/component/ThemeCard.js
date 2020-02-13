063ab2117be8d35f0126388319fc3855456b5727
ValentinAh-Kane <valentin.ah-kane@etu.univ-cotedazur.fr>
2020-02-13 22:48:59 +0100

    .

---
+++ TTO-Client-Touchpad/src/component/ThemeCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+const ThemeCard = props => {
+    const history = useHistory();
+    const theme = props.theme
+    const image = theme.icon
+    const topic = theme.topic;
+    const dispatcher = useDispatch()
+
+    const handleClick = (item) => {
+        dispatcher({ type: 'select_topic', topicSelected: item })
+        history.push({
+            pathname: '/quiz',
+        });
+
+    }
+
+    return (
+        // <div className="quizCard" onClick={() => props.onClick()}>
+        <div className="quizCard" onClick={() => handleClick(theme)}>
+            {theme.icon ? <img className="quizImage" src={"http://10.212.107.151:10000/" + theme.icon} /> :
+                <></>
+            }
+            <h2 className="quizName">{theme.topic}</h2>
+        </div>
+    )
+};
+
+
+export default ThemeCard;
